ba416b480f24f248a4cc37d5c40171e1ceb1567a
Sawyer13 <dgarci13@ucm.es>
2019-03-27 17:43:08 +0100

    Information completed

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Eurodiputados - Art. 13</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Partido Político</th>
+            <th>Eurodiputados</th>
+            <th>Permitir enmiendas</th>
+            <th>Adoptar Ley Copyright</th>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Pilar AYUSO	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Agustín DÍAZ DE MERA GARCÍA CONSUEGRA	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Rosa ESTARÀS FERRAGUT	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <td>Partido Popular	</td>
+            <td>Santiago FISAS AYXELÀ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Esteban GONZÁLEZ PONS	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Luis de GRANDES PASCUAL	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Esther HERRANZ GARCÍA	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Carlos ITURGAIZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Ramón Luis VALCÁRCEL SISO	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Teresa JIMÉNEZ-BECERRIL BARRIO	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Pilar del CASTILLO VERA	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Antonio LÓPEZ-ISTÚRIZ WHITE	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Gabriel MATO	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Francisco José MILLÁN MON	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>José Ignacio SALAFRANCA SÁNCHEZ-NEYRA	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Popular	</td>
+            <td>Verónica LOPE FONTAGNÉ		</td>
+            <td colspan="2" style="text-align: center">Sin información</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español</td>	
+            <td>Clara Eugenia AGUILERA GARCÍA</td>	
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Iratxe GARCÍA PÉREZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Eider GARDIAZABAL RUBIAL	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Inés AYALA SENDER	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr> 
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>José BLANCO LÓPEZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Soledad CABEZÓN RUIZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Jonás FERNÁNDEZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español</td>	
+            <td>Enrique GUERRERO SALOM	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Ramón JÁUREGUI ATONDO	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Inmaculada RODRÍGUEZ-PIÑERO FERNÁNDEZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Juan Fernando LÓPEZ AGUILAR	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Socialista Obrero Español	</td>
+            <td>Sergio GUTIÉRREZ PRIETO		</td>
+            <td colspan="2" style="text-align: center">Sin información</td>
+        </tr>
+        <tr>
+            <td>PODEMOS	</td>
+            <td>Xabier BENITO ZILUAGA	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>PODEMOS	</td>
+            <td>Tania GONZÁLEZ PEÑAS	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>PODEMOS	</td>
+            <td>Lola SÁNCHEZ CALDENTEY	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>PODEMOS	</td>
+            <td>Miguel URBÁN CRESPO	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>PODEMOS	</td>
+            <td>Estefanía TORRES MARTÍNEZ		</td>
+            <td colspan="2" style="text-align: center">Sin información</td>
+        </tr>
+        <tr>
+            <td>Independiente	</td>
+            <td>Beatriz BECERRA BASTERRECHEA	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>Independiente	</td>
+            <td>Enrique CALVET CHAMBON	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Independiente	</td>
+            <td>Javier COUSO PERMUY	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>Independiente	</td>
+            <td>Carolina PUNSET	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Izquierda Unida	</td>
+            <td>Marina ALBIOL GUZMÁN</td>	
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>Izquierda Unida	</td>
+            <td>Paloma LÓPEZ BERMEJO	</td>
+            <td>A favor	</td>
+            <td>Se abstiene</td>
+        </tr>
+        <tr>
+            <td>Esquerra Republicana de Catalunya	</td>
+            <td>Jordi SOLÉ	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>Esquerra Republicana de Catalunya	</td>
+            <td>Josep-Maria TERRICABRAS	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>-	</td>
+            <td>Francesc GAMBÚS	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>-	</td>
+            <td>María Teresa GIMÉNEZ BARBAT	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partido Nacionalista Vasco	</td>
+            <td>Izaskun BILBAO BARANDICA	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Partit dels Socialistes de Catalunya	</td>
+            <td>Javi LÓPEZ		</td>
+            <td colspan="2" style="text-align: center">Sin información</td>
+        </tr>
+        <tr>
+            <td>EQUO	</td>
+            <td>Florent MARCELLESI	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>Bloque Nacionalista Galego	</td>
+            <td>Ana MIRANDA		</td>
+            <td colspan="2" style="text-align: center">Sin información</td>
+        </tr>
+        <tr>
+            <td>Ciudadanos – Partido de la Ciudadanía	</td>
+            <td>Javier NART	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Unión, Progreso y Democracia	</td>
+            <td>Maite PAGAZAURTUNDÚA RUIZ	</td>
+            <td>En contra	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Alternativa galega de esquerda en Europa	</td>
+            <td>Maria Lidia SENRA RODRÍGUEZ	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+        <tr>
+            <td>Partit Demòcrata Europeu i Català</td>	
+            <td>Ramon TREMOSA i BALCELLS	</td>
+            <td>A favor	</td>
+            <td>A favor</td>
+        </tr>
+        <tr>
+            <td>Iniciativa per Catalunya Verds</td>	
+            <td>Ernest URTASUN	</td>
+            <td>A favor	</td>
+            <td>En contra</td>
+        </tr>
+
+        <tr>
+            <td>Izquierda Xunida	</td>
+            <td>Ángela VALLINA		</td>
+            <td colspan="2" style="text-align: center">Sin información</td>
+        </tr>
+
+    </table>
+</body>
+</html>
